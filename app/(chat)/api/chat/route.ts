943ffbd75818a252844827e1123551807dde5fce
SisuMYJ <126390623+SisuMYJ@users.noreply.github.com>
2026-06-07 06:36:10 -0500

    Update route.ts

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -7,14 +7,9 @@ import {
 } from 'ai';
 import { customModel } from '@/lib/ai';
 import { regularPrompt } from '@/lib/ai/prompts';
-import { getWeather } from '@/lib/ai/tools/get-weather';
 import { generateUUID, getMostRecentUserMessage } from '@/lib/utils';
 
 export const maxDuration = 60;
-
-type AllowedTools = 'getWeather';
-const weatherTools: AllowedTools[] = ['getWeather'];
-const allTools: AllowedTools[] = [...weatherTools];
 
 export async function POST(request: Request) {
   const {
@@ -46,11 +41,7 @@ export async function POST(request: Request) {
         system: regularPrompt,
         messages: coreMessages,
         maxSteps: 5,
-        experimental_activeTools: allTools,
         experimental_transform: smoothStream({ chunking: 'word' }),
-        tools: {
-          getWeather,
-        },
         experimental_telemetry: {
           isEnabled: true,
           functionId: 'stream-text',
