3fe530bbb20f9f68aa4fd9a34cde24b3363fa0f7
SisuMYJ <126390623+SisuMYJ@users.noreply.github.com>
2026-06-10 10:58:57 -0500

    Update chat.tsx

--- components/chat.tsx
+++ components/chat.tsx
@@ -36,7 +36,8 @@ export function Chat({
 
   return (
     <div className="flex flex-col min-w-0 h-dvh bg-background">
-      <ChatHeader
+ <ChatHeader
+        chatId={id}
         selectedModelId={currentModelId}
         onModelChange={setCurrentModelId}
       />
